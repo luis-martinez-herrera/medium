d332b2c458f06d2b909b642e13f0b07717eb73ed
luis martinez <luismartinez@outlook.com.pe>
2022-08-10 09:01:16 -0500

    update load tests

--- grpc-vs-rest/load-testing/grpc.js
+++ grpc-vs-rest/load-testing/grpc.js
@@ -2,15 +2,16 @@ import grpc from 'k6/net/grpc';
 import { check, sleep } from 'k6';
 export const options = {
   vus: 10,
-  duration: '2s',
+  duration: '20s',
 };
 
 const client = new grpc.Client();
 client.load(['definitions'], '../../src/main/proto/helloworld.proto');
 
 export default () => {
-  client.connect('grpcbin.localhost:9000', {
-    // plaintext: false
+  client.connect('localhost:9000', {
+    plaintext: true,
+    timeout: 500
   });
 
   const data = {};
@@ -20,7 +21,7 @@ export default () => {
     'status is OK': (r) => r && r.status === grpc.StatusOK,
   });
 
-  console.log(JSON.stringify(response.message));
+  // console.log(JSON.stringify(response.message));
 
   client.close();
   sleep(1);
--- grpc-vs-rest/load-testing/rest.js
+++ grpc-vs-rest/load-testing/rest.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
   vus: 10,
-  duration: '2s',
+  duration: '20s',
 };
 export default function () {
   http.get('http://localhost:8080/hello');
